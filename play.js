95d307eca47cb934071cbd8515dcdc084319ccfb
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-16 08:47:50 -0700

    Added handleUserInput function

--- play.js
+++ play.js
@@ -1,12 +1,25 @@
 
 const { connect } = require('./client');
 
-
-
-
-
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+connect.on("data", handleUserInput);
+
+const handleUserInput = function() {
+  console.log(setupInput);
+};
+
 
 
 
