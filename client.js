4b69d59ce5580474080bfc1a1ae318160ae81ad7
Monica Wolfe <mcwolfe2011@gmail.com>
2023-03-16 08:34:01 -0700

    Added and modify these three files: client.js, constants.js, input.js

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
+
 const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
---
+++ constants.js
@@ -0,0 +1,21 @@
+const { constants } = require("buffer");
+
+const IP = '165.227.47.243';
+const PORT = 50541;
+
+const UPKEY = "Move: up";
+const LEFTKEY = "Move: left";
+const DOWNKEY = "Move: down";
+const RIGHTKEY = "Move: right";
+
+module.exports = {
+  IP,
+  PORT,
+  UPKEY,
+  LEFTKEY,
+  DOWNKEY,
+  RIGHTKEY
+};
+
+
+
